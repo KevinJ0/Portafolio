08ea764333c0c3d98d89ebc10840d2d6319ea21c
Kevin <kevinjooo59@gmail.com>
2019-01-20 14:58:36 -0400

    Menu button javascript & rotate

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,38 @@
- 
+ // select DOM items
+
+
+ $(document).ready(() => {
+
+
+     const menuBtn = $(".menu-btn");
+     const menu = $(".menu");
+     const menuNav = $(".menu-nav");
+     const menuBranding = $(".menu-Branding");
+
+     const navItems = $('.nav-item');
+     let showMenu = false;
+
+    
+
+     menuBtn.click(toggleMenu);
+     function toggleMenu() {
+
+        if (!showMenu) {
+            menuBtn.addClass('close');
+            menu.addClass('show');
+            menuNav.addClass('show');
+            menuBranding.addClass('show');
+            navItems.forEach(item => item.addClass('show'));
+            showMenu = true;
+
+        } else {
+            menuBtn.removeClass('close');
+            menu.removeClass('show');
+            menuNav.removeClass('show');
+            menuBranding.removeClass('show');
+            navItems.forEach(item => item.removeClass('show'));
+            showMenu = false;
+        }
+
+    }
+ });
