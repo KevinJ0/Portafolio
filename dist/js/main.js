1273fb1419760dae951e14c7182443978ea18b62
Kevin <kevinjooo59@gmail.com>
2019-01-27 21:57:34 -0400

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,36 +3,22 @@
 
  $(document).ready(() => {
 
-
+ 
      const menuBtn = $(".menu-btn");
-     const menu = $(".menu");
      const menuNav = $(".menu-nav");
      const menuBranding = $(".menu-Branding");
-
+     let show = true;
      const navItems = $('.nav-item');
-     let showMenu = false;
+     navItems.fadeOut();
+     menuBtn.click(() => {
+         menuBtn.toggleClass('close');
+         $('.menu-side-right').slideToggle();
+         $('.menu-side-left').slideToggle();
+         navItems.addClass('nav-item');
+   
+             show = false;
+             navItems.fadeToggle();
 
-    
+     }); 
 
-     menuBtn.click(toggleMenu);
-     function toggleMenu() {
-
-        if (!showMenu) {
-            menuBtn.addClass('close');
-            menu.addClass('show');
-            menuNav.addClass('show');
-            menuBranding.addClass('show');
-            navItems.forEach(item => item.addClass('show'));
-            showMenu = true;
-
-        } else {
-            menuBtn.removeClass('close');
-            menu.removeClass('show');
-            menuNav.removeClass('show');
-            menuBranding.removeClass('show');
-            navItems.forEach(item => item.removeClass('show'));
-            showMenu = false;
-        }
-
-    }
  });
