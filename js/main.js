9cb3cc5ccca354279be1c00b45440813c9329013
Kevin <kevinjooo59@gmail.com>
2019-03-26 13:26:48 -0400

    Updates

---
+++ js/main.js
@@ -0,0 +1,24 @@
+ // select DOM items
+
+
+ $(document).ready(() => {
+
+ 
+     const menuBtn = $(".menu-btn");
+     const menuNav = $(".menu-nav");
+     const menuBranding = $(".menu-Branding");
+     let show = true;
+     const navItems = $('.nav-item');
+     navItems.fadeOut();
+     menuBtn.click(() => {
+         menuBtn.toggleClass('close');
+         $('.menu-side-right').slideToggle();
+         $('.menu-side-left').slideToggle();
+         navItems.addClass('nav-item');
+   
+             show = false;
+             navItems.fadeToggle();
+
+     }); 
+
+ });
