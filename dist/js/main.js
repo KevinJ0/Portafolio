8821e098203b64df6a5bf223b2240d51a7925c64
Kevin <kevinjooo59@gmail.com>
2019-04-09 15:48:36 -0400

    Translate to spanish, add something about me

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,22 +3,23 @@
 
  $(document).ready(() => {
 
- 
+
      const menuBtn = $(".menu-btn");
      const menuNav = $(".menu-nav");
      const menuBranding = $(".menu-Branding");
      let show = true;
      const navItems = $('.nav-item');
+       
      navItems.fadeOut();
      menuBtn.click(() => {
          menuBtn.toggleClass('close');
          $('.menu-side-right').slideToggle();
          $('.menu-side-left').slideToggle();
          navItems.addClass('nav-item');
-   
-             show = false;
-             navItems.fadeToggle();
 
-     }); 
+         show = false;
+         navItems.fadeToggle();
+
+     });
 
  });
